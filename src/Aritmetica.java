a5d0f74dcfcb9f81b52a140ccb69b94e71b93cc7
Arturo Valdez <santiguardado27@gmail.com>
2025-01-19 16:34:38 -0600

    Clase Aritmetica creacion de constructor

--- src/Aritmetica.java
+++ src/Aritmetica.java
@@ -2,6 +2,13 @@ public class Aritmetica {
     int operando1;
     int operando2;
 
+    //Crear constructor de aritmetica
+    public Aritmetica(int operando1, int operando2){
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+        System.out.println("Ejecutando Constructor");
+    }
+
     void suma(){
         var resultado = operando1 + operando2;
         System.out.println("Resultado suma = " + resultado);
@@ -22,16 +29,17 @@ public class Aritmetica {
     public static void main(String[] args) {
         System.out.println("*** Ejemplo Aritmetica ***");
 
-        var aritmetica1 = new Aritmetica();
-        aritmetica1.operando1 = 5;
-        aritmetica1.operando2 = 7;
+        //El constructor permite agregar los valores directamente a la clase
+        var aritmetica1 = new Aritmetica(5,7);
+        //aritmetica1.operando1 = 5;
+        //aritmetica1.operando2 = 7;
         aritmetica1.suma();
         aritmetica1.resta();
         aritmetica1.multiplicacion();
         aritmetica1.divicion();
         System.out.println();
 
-        //Crear nuevo objeto
+        /*Crear nuevo objeto
         var aritmetica2 = new Aritmetica();
         aritmetica2.operando1 = 12;
         aritmetica2.operando2 = 16;
@@ -39,6 +47,8 @@ public class Aritmetica {
         aritmetica2.resta();
         aritmetica2.multiplicacion();
         aritmetica2.divicion();
+        */
+
 
     }
 
