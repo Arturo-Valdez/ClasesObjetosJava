d8361e6f264b903c6e5d545e569266a85f6b910a
Arturo Valdez <santiguardado27@gmail.com>
2025-01-19 16:07:53 -0600

    ClasesJava

---
+++ src/Persona.java
@@ -0,0 +1,14 @@
+public class Persona {
+    String nombre;
+    String apellido;
+    String email;
+    String tel;
+
+    void mostrarPersona(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Email: " + email);
+        System.out.println("Numero Celular: " + tel);
+    }
+}
+
