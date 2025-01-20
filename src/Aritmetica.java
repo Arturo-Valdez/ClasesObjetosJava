88c16e2eaf88ef1b24ccf0e23ed57edfb86d1bd1
Arturo Valdez <santiguardado27@gmail.com>
2025-01-19 21:13:44 -0600

    Clase Aritmetica uso de la variable de referencia this

--- src/Aritmetica.java
+++ src/Aritmetica.java
@@ -1,28 +1,34 @@
 public class Aritmetica {
+    //Atributos de clase
     int operando1;
     int operando2;
 
+    //Constructor vacio permite que no sea necesario agregar valores obligatorios
+    public Aritmetica(){
+        System.out.println("Constructor vacio");
+    }
+
     //Crear constructor de aritmetica
     public Aritmetica(int operando1, int operando2){
+        System.out.println("Ejecutando Constructor");
         this.operando1 = operando1;
         this.operando2 = operando2;
-        System.out.println("Ejecutando Constructor");
     }
 
     void suma(){
-        var resultado = operando1 + operando2;
+        var resultado = this.operando1 + this.operando2;
         System.out.println("Resultado suma = " + resultado);
     }
     void resta(){
-        var resultado = operando1 - operando2;
+        var resultado = this.operando1 - this.operando2;
         System.out.println("Resultado resta = " + resultado);
     }
     void multiplicacion(){
-        var resultado = operando1 * operando2;
+        var resultado = this.operando1 * this.operando2;
         System.out.println("Resultado multiplicacion = " + resultado);
     }
     void divicion(){
-        var resultado = operando1 / operando2;
+        var resultado = this.operando1 / this.operando2;
         System.out.println("Resultado divicion = " + resultado);
     }
 
@@ -38,6 +44,14 @@ public class Aritmetica {
         aritmetica1.multiplicacion();
         aritmetica1.divicion();
         System.out.println();
+
+
+        //Constructor vacio permite que no sea necesario agregar valores obligatorios
+        var aritmetica2 = new Aritmetica();
+        aritmetica2.suma();
+        aritmetica2.resta();
+        aritmetica2.multiplicacion();
+        //aritmetica2.divicion();
 
         /*Crear nuevo objeto
         var aritmetica2 = new Aritmetica();
